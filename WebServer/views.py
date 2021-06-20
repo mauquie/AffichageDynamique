af1d47da70907c52036df024fb8763f24508e781
Elowarp <agent.craftien@gmail.com>
2021-06-20 21:19:13 +0200

    Ajout des dossiers pour les templates

--- WebServer/views.py
+++ WebServer/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+#Variable de debug car il n'y a pas encore de db
+GROUPE = "ADMIN"
 
 # Create your views here.
 
 def index(request):
-    return JsonResponse({"text": "Page d'administration !"})
+    return render(request, 'WebServer/index.html')
