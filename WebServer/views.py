13d2b076f0e741b5501d1a3f42bd2a2c14b5f1f4
Elowarp <agent.craftien@gmail.com>
2021-06-22 18:05:53 +0200

    Ajout Templates

--- WebServer/views.py
+++ WebServer/views.py
@@ -7,4 +7,4 @@ GROUPE = "ADMIN"
 # Create your views here.
 
 def index(request):
-    return render(request, 'WebServer/index.html')
+    return render(request, 'WebServer/index.html', {"group": GROUPE})
