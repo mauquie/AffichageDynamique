e4f76e614524e18aab2fdf42d2289ed665ebacf2
Elowarp <agent.craftien@gmail.com>
2021-06-25 01:20:16 +0200

    Ajout page d'accueil + page gestion des articles

--- WebServer/urlsWebServ.py
+++ WebServer/urlsWebServ.py
@@ -4,5 +4,9 @@ from . import views as urls
 
 #Urls concernant le serveur Web (celui utilisé par des êtres humains)
 urlpatterns = [
-    path("", urls.index)
+    path("", urls.index),
+    path("articles", urls.articles),
+    path("articles/ajouter", urls.ajouterArticle),
+    path("articles/modifier", urls.modifierArticle),
+    path("articles/supprimer", urls.supprimerArticle),
 ]
--- WebServer/views.py
+++ WebServer/views.py
@@ -1,10 +1,59 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-#Variable de debug car il n'y a pas encore de db
-GROUPE = "ADMIN"
+#Variable de debug
+GROUPE = "STUDENT"
 
-# Create your views here.
+def index(request, message=""):
+    ''' 
+        Fonction appelée quand on veut la page initiale où l'on retrouve les actions rapides, et un lien
+        vers le reste des actions possibles sur le site.
+    '''
+    return render(request, 'WebServer/index.html', exInfos("Accueil", {"user_group": GROUPE}, message))
 
-def index(request):
-    return render(request, 'WebServer/index.html', {"group": GROUPE})
+def articles(request, message=""):
+    '''
+        Fonction appelée quand on veut intéragir d'une quelconque façon avec les articles (ajouter, modifier, supprimer)
+        Elle contient une liste des 5 derniers articles postés avec des boutons pour modifier et supprimer un article
+        et un autre lien pour créer un article.
+    '''
+    return render(request, 'WebServer/Articles/index.html', exInfos("Articles", {"user_group": GROUPE}, message))
+
+def ajouterArticle(request, message=""):
+    '''
+        Fonction appelé quand on veut écrire un article.
+    '''
+    return render(request, 'WebServer/Articles/ajouter.html', exInfos("Ajouter un article", {"user_group": GROUPE}, message))
+
+def modifierArticle(request, message=""):
+    '''
+        Fonction appelé quand on veut modifier un article.
+    '''
+    return render(request, 'WebServer/Articles/modifier.html', exInfos("Modifier un article", {'user_group': GROUPE}, message))
+
+def supprimerArticle(request, message=""):
+    '''
+        Fonction appelé quand on veut supprimer un article
+    '''
+    message = "Supprimé avec succès !"
+    return articles(request, message)
+
+
+def exInfos(pageTitle, user, message=""):
+    '''
+        Fonction appelé quand on veut envoyer des données complémentaires aux Templates comme par exemple le groupe 
+        de l'utilisateur ou un message d'information sur une action effectuée
+
+        @Params :
+            pageTitle : (string) Nom de la page
+            user : (dict) Dictionnaire d'infos sur l'utilisateur
+            ?message : (string) Message à transmettre à l'utilisateur sur la page
+
+        @Return :
+            dict : Informations complémentaires
+    '''
+    return {
+        "pageTitle": pageTitle,
+        "user": user,
+        "message": message,
+    }
