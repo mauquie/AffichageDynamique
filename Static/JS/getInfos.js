336fb7ad73a660eb4db63cb6cfeaa330d4633176
RaptorJesus82 <mbetous82@gmail.com>
2021-10-06 07:26:53 +0200

    ajout roulement des infos

--- Static/JS/getInfos.js
+++ Static/JS/getInfos.js
@@ -4,14 +4,19 @@ setInterval(() => {
 
 getInformations();
 
+index=0
 function getInformations() {
     fetch("/api/infos").then(response => {
         return response.json();
 
     }).then(data => {
         var domInfo = document.getElementById("information");
-
-        information = "Information " + data[0].type.name + " : " + data[0].message;
+        information = "Information " + data[index].type.name + " : " + data[index].message;
         domInfo.innerText = information;
+        if (index==data.length-1)
+        {
+            index = -1
+        }
+        index++
     });
 }
