ccdae9b1d32caed7a171e07382e41b9ca2af8f7d
RaptorJesus82 <mbetous82@gmail.com>
2021-10-21 20:16:58 +0200

    ajout anim

--- Static/JS/getInfos.js
+++ Static/JS/getInfos.js
@@ -1,18 +1,11 @@
-var text = document.getElementById("information") //Création d'un span pour mesurer la longeur du
-var documentMesure = document.createElement("span") //texte d'information.
-document.body.append(documentMesure)
-documentMesure.style.cssText = text.style.cssText
-documentMesure.style.position = "absolute"
-documentMesure.style.top = "-200px"
-
-
 index=0
 function changeInfo(listeInfo)
 {
     var domInfo = document.getElementById("information")
     if (listeInfo.length == 0)
     {
-        domInfo.hidden = true
+        var domInfoCachable = document.getElementById("container-cachable")
+        domInfoCachable.hidden = true
     }
     else
     {
@@ -23,7 +16,8 @@ function changeInfo(listeInfo)
         }
         var information = listeInfo[index].message;
         domInfo.innerHTML = information
-        domInfo.style.fontSize="80px"
+        domInfo.style.fontSize="80px" //Taille de base de l'info, adaptée après par setSize().
+        changeStyleInfo(listeInfo[index])
         index++
     }
 }
@@ -36,20 +30,77 @@ function getInformations()
     }).then(data => {
         changeInfo(data)
         setSize()
+        playAnimation()
     });
 }
 
 function playAnimation()
+/*{
+    var domInfoContainer = document.getElementById("infoContainer")
+    var tl = anime.timeline({
+        targets: domInfoContainer
+    })
+    tl.add(
+    {
+        translateY: [-domInfoContainer.clientHeight*2,0],
+        easing: 'spring(1, 80, 10, 0)',
+    })
+    tl.add(
+    {
+        delay: 13000,
+        easing: 'easeInElastic(1, 2)',
+        translateX: domInfoContainer.clientWidth*2,
+    })
+    tl.add({
+        translateX: 0,
+        translateY: -domInfoContainer.clientHeight*2,
+        easing: 'steps(1)',
+    }
+    )
+
+}
+*/
+/*
 {
-    var textInfo = document.getElementById("information")
-    documentMesure.innerHTML = text.innerHTML
-    textLength = documentMesure.offsetWidth
-    anime({
-        targets: textInfo,
-        translateX: [-textLength, window.innerWidth],
-        duration: (textLength+window.innerWidth)*10,
-        easing: "linear",
-        direction: "reverse",
+    var domInfoContainer = document.getElementById("infoContainer")
+    var tl = anime.timeline({
+        targets: domInfoContainer
+    })
+    tl.add(
+    {
+        translateX: [-domInfoContainer.clientWidth*2,0],
+        easing: 'spring(1, 80, 10, 0)',
+    })
+    .add(
+    {
+        delay: 13000,
+        easing: 'easeInElastic(1, 2)',
+        translateX: domInfoContainer.clientWidth*2,
+    })
+    .add({
+        translateX: -domInfoContainer.clientWidth*2,
+        easing: 'steps(1)',
+    })
+}*/
+{
+    var domInfoContainer = document.getElementById("infoContainer")
+    var tl = anime.timeline({
+        targets: domInfoContainer
+    })
+    tl.add(
+    {
+        translateX: [-domInfoContainer.clientWidth*2,0],
+        easing: 'spring(1, 80, 10, 0)',
+    })
+    .add(
+    {
+        delay: 16000,
+        easing: 'easeInElastic(1, 2)',
+        translateX: domInfoContainer.clientWidth*2,
+    })
+    .add({
+        translateX: -domInfoContainer.clientWidth*2,
+        easing: 'steps(1)',
     })
 }
 
@@ -59,14 +110,36 @@ function setSize()
     pInfoDom = document.getElementById("information")
     if (pInfoDom.clientHeight > domContainerInfo.clientHeight)
     {
-        fontSize = window.getComputedStyle(pInfoDom).fontSize
-        fontSize = Number(fontSize.slice(0, fontSize.length-2))
-        pInfoDom.style.fontSize = parseFloat(fontSize-2) + "px"
+        fontSize = window.getComputedStyle(pInfoDom).fontSize //nécessaire pour transformer 
+        fontSize = Number(fontSize.slice(0, fontSize.length-2))//"taille en px" en taille int
+        pInfoDom.style.fontSize = parseFloat(fontSize-2) + "px"//puis repasser en "taille en px"
         setSize()
     }
 }
 
+ 
+function changeStyleInfo(info)
+{ 
+    var domInfoContainer = document.getElementById("infoContainer")
+    console.log(info.type.id)
+    switch(info.type.id) {
+        case 1:
+            domInfoContainer.classList.add("bg-info")
+            break;
+        case 2:
+            domInfoContainer.style.backgroundColor = "blue"
+            break;
+        default:
+           break;
+      }
+}
+
+/*anime.set({ //nécessaire, il faut que l'info initiallement au dessus de l'écran.
+    targets: document.getElementById("infoContainer"),
+    translateX: -(document.getElementById("infoContainer").clientWidth*2),
+})
+*/ 
 getInformations()
 setInterval(() => {
     getInformations()
-}, 5000)
+}, 19000)
