6175aedd35b57b3a71d0cd9d28be828fd655841b
elowarp <agent.craftien@gmail.com>
2021-10-23 13:50:16 +0200

    Ajout Menu sur la page du self

--- ApiServer/pronote.py
+++ ApiServer/pronote.py
@@ -99,65 +99,68 @@ def refreshMenu():
 
         #Parsing des données reçu
         if requete.status_code == 200:
-            menus = requete.json()["data"]["menu"][0]
+            #Si la requète nous renvoie bien un menu (ce qu'elle ne fait pas si on est Samedi par exemple)
+            # car le samedi il n'y a pas de self
+            if len(requete.json()["data"] > 0):
+                menus = requete.json()["data"]["menu"][0]
 
-            #Pour chaque menu d'aujourd'hui
-            for IDMenu in range(len(menus["meals"])):
-                menu = menus["meals"][IDMenu]
+                #Pour chaque menu d'aujourd'hui
+                for IDMenu in range(len(menus["meals"])):
+                    menu = menus["meals"][IDMenu]
 
-                #On sait que le premier menu est le menu du midi, or le premier index d'une liste est 0
-                # Donc 0 est divisible par 2 donc la condition est vraie (et cest le menu du midi) sinon la condition
-                # est fausse donc ce n'est pas le menu du midi, cest à dire que c'est celui du soir
-                isRepasMidi = IDMenu % 2 == 0
+                    #On sait que le premier menu est le menu du midi, or le premier index d'une liste est 0
+                    # Donc 0 est divisible par 2 donc la condition est vraie (et cest le menu du midi) sinon la condition
+                    # est fausse donc ce n'est pas le menu du midi, cest à dire que c'est celui du soir
+                    isRepasMidi = IDMenu % 2 == 0
 
-                #Convertion de la date donnée par pronote(Epoch?) en une date compréhensible par la BDD 
-                dateToday = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=menus["date"]/1000 + 7200)
+                    #Convertion de la date donnée par pronote(Epoch?) en une date compréhensible par la BDD 
+                    dateToday = datetime.datetime(1970, 1, 1) + datetime.timedelta(seconds=menus["date"]/1000 + 7200)
 
 
-                #Vérification que le repas n'existe pas déjà dans la BDD
-                repasObjects = Repas.objects.filter(repas_midi = isRepasMidi, date = dateToday)
+                    #Vérification que le repas n'existe pas déjà dans la BDD
+                    repasObjects = Repas.objects.filter(repas_midi = isRepasMidi, date = dateToday)
 
-                repas = None
+                    repas = None
 
-                #Si on ne trouve pas de repas à la même date dans la bdd
-                if len(repasObjects) == 0:
-                    #Initialisation du nouvel objet Repas
-                    repas = Repas()
-                    repas.repas_midi = isRepasMidi
-                    repas.date = dateToday
+                    #Si on ne trouve pas de repas à la même date dans la bdd
+                    if len(repasObjects) == 0:
+                        #Initialisation du nouvel objet Repas
+                        repas = Repas()
+                        repas.repas_midi = isRepasMidi
+                        repas.date = dateToday
+                        repas.save()
+
+                    else: #Il y a deja un repas à la même date 
+                        # On utilise ce repas que l'on vide de tout aliment pour pouvoir les réattribuer si
+                        #  on a changé la composition du repas à la dernière minute 
+                        repas = repasObjects[0]
+                        repas.aliments_du_repas.clear()
+
+
+                    #Pour chaque partie de repas (Entrée, Viande, Dessert, etc)
+                    for IDpartieRepas in range(len(menu)):
+
+                        #Pour chaque Aliment dans cette partie
+                        for aliment in menu[IDpartieRepas]:
+                            #Vérification qu'il n'y ait pas déjà l'aliment dans la BDD avec le même nom et la même partie du repas
+                            alimentObjects = Aliment.objects.filter(name = aliment["name"], partie_du_repas = IDpartieRepas + 1)
+
+                            #S'il n'y en a pas dans la bdd
+                            if len(alimentObjects) == 0:
+                                #On crée un nouvel aliment 
+                                currentAliment = Aliment()
+                                currentAliment.name = aliment["name"]
+                                currentAliment.partie_du_repas = PartieDuRepas.objects.get(pk=IDpartieRepas+1)
+                                currentAliment.save()
+
+                                #Et on l'ajoute au repas que l'on est en train de construire
+                                repas.aliments_du_repas.add(currentAliment)
+                            
+                            else: #S'il existe déjà dans la bdd 
+                                #On l'ajoute simplement au repas que l'on est en train de construire
+                                repas.aliments_du_repas.add(alimentObjects[0])
+
+                    #Sauvegarde dans la base de données
                     repas.save()
 
-                else: #Il y a deja un repas à la même date 
-                    # On utilise ce repas que l'on vide de tout aliment pour pouvoir les réattribuer si
-                    #  on a changé la composition du repas à la dernière minute 
-                    repas = repasObjects[0]
-                    repas.aliments_du_repas.clear()
-
-
-                #Pour chaque partie de repas (Entrée, Viande, Dessert, etc)
-                for IDpartieRepas in range(len(menu)):
-
-                    #Pour chaque Aliment dans cette partie
-                    for aliment in menu[IDpartieRepas]:
-                        #Vérification qu'il n'y ait pas déjà l'aliment dans la BDD avec le même nom et la même partie du repas
-                        alimentObjects = Aliment.objects.filter(name = aliment["name"], partie_du_repas = IDpartieRepas + 1)
-
-                        #S'il n'y en a pas dans la bdd
-                        if len(alimentObjects) == 0:
-                            #On crée un nouvel aliment 
-                            currentAliment = Aliment()
-                            currentAliment.name = aliment["name"]
-                            currentAliment.partie_du_repas = PartieDuRepas.objects.get(pk=IDpartieRepas+1)
-                            currentAliment.save()
-
-                            #Et on l'ajoute au repas que l'on est en train de construire
-                            repas.aliments_du_repas.add(currentAliment)
-                        
-                        else: #S'il existe déjà dans la bdd 
-                            #On l'ajoute simplement au repas que l'on est en train de construire
-                            repas.aliments_du_repas.add(alimentObjects[0])
-
-                #Sauvegarde dans la base de données
-                repas.save()
-
-    
+        
