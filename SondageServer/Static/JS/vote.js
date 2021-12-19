14af370fffe2b9099591c16183b60a9110e0e91d
RaptorJesus <mbetous82@gmail.com>
2021-12-19 04:19:04 +0100

    ajout coloration boutons sondage + fix article trop grand

--- SondageServer/Static/JS/vote.js
+++ SondageServer/Static/JS/vote.js
@@ -1,5 +1,7 @@
 DOMquestion = document.getElementById("question")
 DOMlistAnswers = document.getElementById("answers")
+list_buttons = []
+list_labels = []
 
 function createButton(textAnswer, idAnswer, answerIndex){
     divButton = document.createElement("div")
@@ -11,12 +13,15 @@ function createButton(textAnswer, idAnswer, answerIndex){
     button.setAttribute("name", "vote") 
     button.setAttribute("id", "option" + String(answerIndex))
     button.setAttribute("value", idAnswer)
+    list_buttons.push(button)
+    button.addEventListener("click", toggleButton)
 
 
     label = document.createElement("label")
-    label.className = "btn vote-input"
+    label.className = "btn vote-input shadow-none"
     label.setAttribute("for", "option" + String(answerIndex))
     label.innerText = textAnswer
+    list_labels.push(label)
 
     DOMlistAnswers.appendChild(divButton)
     divButton.appendChild(button)
@@ -47,4 +52,20 @@ else
 function trySubmit(){
     var myModal = new bootstrap.Modal(document.getElementById('modal'), {})
     myModal.show()
+}
+
+function toggleButton()
+{
+    for (i=0; i<list_buttons.length; i++)
+    {
+        if (list_buttons[i].checked == true)
+        {
+            list_labels[i].style.background = "#FF83044D"
+
+        }
+        else
+        {
+            list_labels[i].style.background = "white"
+        }
+    }
 }
