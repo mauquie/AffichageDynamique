9f093b4df2e2ed5e2363e843a0fd5e782be0ae50
RaptorJesus <mbetous82@gmail.com>
2021-12-20 01:34:28 +0100

    fix info taille

--- AffichageDynamiqueServer/Static/JS/getInfos.js
+++ AffichageDynamiqueServer/Static/JS/getInfos.js
@@ -52,15 +52,7 @@ function getInformations()
 function changeInfo(listeInfo)
 {
     domInfo.innerHTML = listeInfo[indexInformations].message; 
-    domInfo.style.fontSize="25vh" //Taille de base de l'info, adaptée après par setSize().
-    if (listeInfo[indexInformations].type.id == 1){ //Si le type est "Important"
-        domInfo.style.color = "#dc3545"
-        domInfo.style.textShadow = "1px 1px 4px rgba(0, 0, 0, 0.3)"
-
-    } else { //Sinon si le type est "Lambda"
-        domInfo.style.color = "#212529"
-        domInfo.style.textShadow = ""
-    }
+    domInfo.style.fontSize="4vh" //Taille de base de l'info, adaptée après par setSize().
 }
 
 function setSize()
@@ -71,7 +63,8 @@ function setSize()
     {
         fontSize = window.getComputedStyle(pInfoDom).fontSize //nécessaire pour transformer 
         fontSize = Number(fontSize.slice(0, fontSize.length-2))//"taille px" en une taille en int
-        pInfoDom.style.fontSize = parseFloat(fontSize-0.02) + "vh"//puis repasser en "taille px"
+        fontSizeVH = (fontSize/window.innerHeight)*100 //puis en taille vh
+        pInfoDom.style.fontSize = parseFloat(fontSizeVH-0.1) + "vh"//puis repasser en "taille px"
         setSize()
     }
 }
