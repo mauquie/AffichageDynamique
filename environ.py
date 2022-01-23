c9c1db7e20420723c727a3cfc58232b28db73e94
Elowarp <agent.craftien@gmail.com>
2022-01-23 17:42:33 +0100

    Edit environ

--- environ.py
+++ environ.py
@@ -1,7 +1,7 @@
 def getEnv(location = ".env"):
     """
     Lit le fichier .env dans le repertoire choisi et renvoie un dictionnaire
-    avec les valeurs
+    avec les valeurs. Si il n'est pas trouvé alors il renvoie un dictionnaire vide
 
     Args:
         location (str, optionnel): Localisation du .env
@@ -10,26 +10,31 @@ def getEnv(location = ".env"):
         dict: Dictionnaire avec les valeurs
     """
     #Ouverture du fichier
-    env = open(location)
+    try:
+    	env = open(location)
     
-    #Lecture des lignes => Liste des lignes
-    envListBase = env.readlines()
+    except FileNotFoundError:
+    	return {}
+    	
+    else:
+        #Lecture des lignes => Liste des lignes
+	 envListBase = env.readlines()
 
-    envDict = {}
+        envDict = {}
 
-    #Pour chaque ligne
-    for line in envListBase:
-        #On enleve le retour à la ligne s'il y en a un
-        line = line.split("\n")[0]
+        #Pour chaque ligne
+        for line in envListBase:
+            #On enleve le retour à la ligne s'il y en a un
+	    line = line.split("\n")[0]
 
-        #Si la ligne n'est pas vide
-        if line != "":
-            #On separe la ligne en deux au niveau de =
-            line = line.split("=")
+	    #Si la ligne n'est pas vide
+	    if line != "":
+	    #On separe la ligne en deux au niveau de =
+	    line = line.split("=")
 
-            #On affecte le nom de la variable à la valeur
-            # De : DB_PORT = 5432
-            # A : {"DB_PORT": 5432}
-            envDict[line[0]] = line[1]
+	    #On affecte le nom de la variable à la valeur
+	    # De : DB_PORT = 5432
+	    # A : {"DB_PORT": 5432}
+	    envDict[line[0]] = line[1]
 
-    return envDict
+	    return envDict
