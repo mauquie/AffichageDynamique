d359fc7b40dd5e1d8cd09cd9fc9c6f956085b599
Elowarp <agent.craftien@gmail.com>
2022-01-25 11:21:20 +0100

    fix environ

--- environ.py
+++ environ.py
@@ -27,14 +27,14 @@ def getEnv(location = ".env"):
             #On enleve le retour à la ligne s'il y en a un
             line = line.split("\n")[0]
 
-	    #Si la ligne n'est pas vide
+	        #Si la ligne n'est pas vide
             if line != "":
                 #On separe la ligne en deux au niveau de =
                 line = line.split("=")
 
-	    #On affecte le nom de la variable à la valeur
-	    # De : DB_PORT = 5432
-	    # A : {"DB_PORT": 5432}
+            #On affecte le nom de la variable à la valeur
+            # De : DB_PORT = 5432
+            # A : {"DB_PORT": 5432}
             envDict[line[0]] = line[1]
 
-            return envDict
+        return envDict
