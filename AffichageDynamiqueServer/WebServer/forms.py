67dc8ff4fe87c8580622940f2feff25ed8152fbb
Elowarp <agent.craftien@gmail.com>
2022-01-29 21:23:46 +0100

    Commenting fForms.py

--- AffichageDynamiqueServer/WebServer/forms.py
+++ AffichageDynamiqueServer/WebServer/forms.py
@@ -13,6 +13,10 @@ class UserForm(forms.ModelForm):
     groups = forms.ModelChoiceField(queryset=Group.objects.all(), initial=1, widget=forms.Select(attrs={'class': 'form-select my-2'}))
 
 class changeOwnAccount(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la modification du compte de l'user lui
+    même 
+    """
     class Meta:
         model = models.Users
         fields = ["username", "email", "profile_picture", "first_name", "last_name"]
@@ -21,6 +25,10 @@ class changeOwnAccount(forms.ModelForm):
     profile_picture = forms.ImageField(required=False)
 
 class changeOthersAccount(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la modification d'un compte autre que 
+    celui de l'user 
+    """
     class Meta:
         model = models.Users
         fields = ["username", "first_name", "last_name", "email", "profile_picture", "groups"]
@@ -29,6 +37,9 @@ class changeOthersAccount(forms.ModelForm):
     profile_picture = forms.ImageField(required=False)
 
 class ArticleForm(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la création/modification d'un article
+    """
     class Meta: 
         model = models.Articles
         fields = ["title", "content", "image", "date_end", "is_shown"]
@@ -36,16 +47,25 @@ class ArticleForm(forms.ModelForm):
     image = forms.ImageField(required=False, widget=forms.FileInput(attrs={"class": "form-control", "id": "imageInput", "hidden": ""}))
 
 class InformationForm(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la création/modification d'une info
+    """
     class Meta:
         model = models.Informations
         fields = ["type", "message", "date_end", "is_shown"]
 
 class ScreenForm(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la création/modification d'un écran
+    """
     class Meta:
         model = models.Screens
         fields = ["name", "code_name"]
 
 class PageForm(forms.ModelForm):
+    """
+    Classe contenant le formulaire pour la création/modification d'une page
+    """
     class Meta:
         model = models.Pages
         fields = ["description", "filename"]
